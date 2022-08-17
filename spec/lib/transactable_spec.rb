95e55f90fba539c7bbeeade494950942aca8883c
Brooke Kuhlmann <brooke@alchemists.io>
2022-08-17 11:07:46 -0600

    Added transactable module inclusion
    Necessary so one can quickly make an object *transactable* by simple
    inclusion. Examples:
    
        # Default
        class Demo
          include Transactable
        end
    
        # Custom
        class Demo
          include Transactable.with(MySteps)
        end
    
    This provides syntax most people are use to while allowing further
    customization if desired.

---
+++ spec/lib/transactable_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "dry/container"
+
+RSpec.describe Transactable do
+  describe ".included" do
+    it "includes behavior" do
+      implementation = Class.new.include described_class
+      expect(implementation.ancestors).to include(described_class)
+    end
+  end
+
+  describe ".with" do
+    it "includes default behavior" do
+      implementation = Class.new.include described_class.with
+      expect(implementation.ancestors.join(", ")).to include(described_class.to_s)
+    end
+
+    it "includes custom behavior" do
+      container = Dry::Container.new
+      container.register(:echo) { -> result { result } }
+      implementation = Class.new.include described_class.with(container)
+
+      expect(implementation.ancestors.join(", ")).to include("Transactable::Pipeable")
+    end
+  end
+end
