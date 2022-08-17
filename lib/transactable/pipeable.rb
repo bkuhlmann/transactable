820003238e0b82a08f9f19f39664ae5dbb90f5ac
Brooke Kuhlmann <brooke@alchemists.io>
2022-08-17 10:42:59 -0600

    Added pipeable
    Necessary to teach an object how to behave line a pipeline of
    transactions so any object that wants to have transaction-like
    behavior can do so via the `#pipe` function.
    
    Reek was updated to ignore the nested iterator and too many statement
    violations since -- when metaprogramming like this -- this is hard to
    avoid without making the code more complex by splitting things up
    further. At least all logic is self-contained as best as possible.

---
+++ lib/transactable/pipeable.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "dry/monads/result"
+
+module Transactable
+  # Allows any object to pipe sequential steps together which can be composed into a single result.
+  class Pipeable < Module
+    def initialize steps = Steps::Container
+      super()
+      @steps = steps
+      @instance_module = Class.new(Module).new
+    end
+
+    def included klass
+      super
+      define_pipe
+      define_steps
+      klass.include instance_module
+    end
+
+    private
+
+    attr_reader :steps, :instance_module
+
+    def define_pipe
+      instance_module.define_method :pipe do |input, *steps|
+        fail ArgumentError, "Transaction must have at least one step." if steps.empty?
+
+        result = input.is_a?(Dry::Monads::Result) ? input : Dry::Monads::Success(input)
+
+        steps.reduce(&:>>).call result
+      rescue NoMethodError
+        raise TypeError, "Step must be functionally composable and answer a monad."
+      end
+    end
+
+    def define_steps
+      instance_module.class_exec steps do |container|
+        container.each_key do |name|
+          define_method name do |*positionals, **keywords, &block|
+            step = container[name]
+            step.is_a?(Proc) ? step : step.new(*positionals, **keywords, &block)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/transactable/pipeable_spec.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "dry/monads/result"
+
+RSpec.describe Transactable::Pipeable do
+  subject(:pipeable) { implementation.new }
+
+  describe "#call" do
+    context "with single step (primitive)" do
+      let :implementation do
+        Class.new do
+          include Transactable::Pipeable.new
+
+          def call(input) = pipe input, as(:to_s)
+        end
+      end
+
+      it "answers result" do
+        result = pipeable.call "test"
+        expect(result.success).to eq("test")
+      end
+    end
+
+    context "with single step (monad)" do
+      let :implementation do
+        Class.new do
+          include Transactable::Pipeable.new
+
+          def call(input) = pipe Dry::Monads::Success(input), as(:to_s)
+        end
+      end
+
+      it "answers result" do
+        result = pipeable.call "test"
+        expect(result.success).to eq("test")
+      end
+    end
+
+    context "with a custom Proc step" do
+      let :implementation do
+        Class.new do
+          include Transactable::Pipeable.new({echo: -> result { result }})
+
+          def call(input) = pipe input, echo
+        end
+      end
+
+      it "answers result" do
+        result = pipeable.call "test"
+        expect(result.success).to eq("test")
+      end
+    end
+
+    context "with multiple steps" do
+      let :implementation do
+        Class.new do
+          include Transactable::Pipeable.new
+
+          def call(input) = pipe input, insert("a", at: 0), insert("b")
+        end
+      end
+
+      it "answers result" do
+        result = pipeable.call "test"
+        expect(result.success).to eq(%w[a test b])
+      end
+    end
+
+    context "with step that is not a Proc or a Class" do
+      let :implementation do
+        Class.new do
+          include Transactable::Pipeable.new
+
+          def call(input) = pipe input, Object.new
+        end
+      end
+
+      it "fails with invalid type" do
+        expectation = proc { pipeable.call "test" }
+        expect(&expectation).to raise_error(TypeError, /functionally composable.+monad/)
+      end
+    end
+
+    context "with no steps" do
+      let :implementation do
+        Class.new do
+          include Transactable::Pipeable.new
+
+          def call(input) = pipe input
+        end
+      end
+
+      it "fails with argument error" do
+        expectation = proc { pipeable.call "test" }
+        expect(&expectation).to raise_error(ArgumentError, /must have at least one step/)
+      end
+    end
+  end
+end
