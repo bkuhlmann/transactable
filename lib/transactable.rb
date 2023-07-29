706f4cec7a18aec6e199bfaa3b84f544ab7b095d
Brooke Kuhlmann <brooke@alchemists.io>
2023-07-29 09:47:27 -0600

    Refactored module included terminology to use descendant
    Necessary to improve readability.

--- lib/transactable.rb
+++ lib/transactable.rb
@@ -6,7 +6,7 @@ Zeitwerk::Loader.for_gem.setup
 
 # Main namespace.
 module Transactable
-  def self.included(klass) = klass.include Pipeable.new
+  def self.included(descendant) = descendant.include Pipeable.new
 
   def self.with(...) = Pipeable.new(...)
 end
